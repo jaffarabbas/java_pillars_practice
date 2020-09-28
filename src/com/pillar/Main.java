b14e10ae302a698feccdede88a905e79918b1a9c
jaffarabbas <gamaportal8@gmail.com>
2020-09-28 23:42:18 +0500

    28-9-2020

--- src/com/pillar/Main.java
+++ src/com/pillar/Main.java
@@ -5,29 +5,49 @@ package com.pillar;
 
 //inheritance
 
+//2-Multilevel inheritance
+
+//3-hiraical inheritance
+
 class parent{
-    String father="sdfsdfds",mother="sdfsdfsd";
+    String father="sdfsdfds",mother="sdfsdfsd";//instance
     void home(){
         System.out.println(father+"\n"+mother+"yes they are ");
     }
 }
 
 class child extends parent{
-    child(){
-        home();
-        System.out.println("Confrim !!!!"+father+"\n"+mother);
-    }
+//    child(){
+//        home();
+//        System.out.println("Confrim !!!!"+father+"\n"+mother);
+//    }
+     void child_kivasyiat_from_parent(){
+         System.out.println("tum bahar niklo tumahar koi kam nahi !!!! from father : "+father+"\nor un ki sarikyhiyat : "+mother);
+     }
 }
 
+class child_kai_bachy  extends parent {
+//    child_kai_bachy(){
+//        System.out.println("Grand father : "+father+"\nGrand Mother :"+mother);
+//    }
+    void potoKivasiyatBydada(){
+        System.out.println("Grand father : "+father+"\nGrand Mother :"+mother+"Un logo ne jidad imran khan ko de hai or app ko bola hia bijlii ka bil bar do");
+    }
+}
 class Nadra{
     void bayform(){
-        child samin = new child();
+        child samin_kai_garvay = new child();
+        child_kai_bachy samin = new child_kai_bachy();
+
+        samin_kai_garvay.child_kivasyiat_from_parent();
+        samin.potoKivasiyatBydada();
     }
 }
 
 public class Main {
 
     public static void main(String[] args) {
-
+       Nadra obj = new Nadra();
+       obj.bayform();
     }
 }
